8bdd54c40cbc48a85ccd919158b750e1fda06bf9
David Basto <davidbasto01@gmail.com>
2022-07-02 15:18:21 -0500

    update: converter styles

--- src/components/select-currency.jsx
+++ src/components/select-currency.jsx
@@ -26,24 +26,24 @@ export default function SelectCurrency() {
       <Listbox value={selected} onChange={setSelected}>
         <div className="relative mt-1">
           <Listbox.Button className="relative w-full cursor-default rounded bg-white py-4 pl-3 pr-10 text-left border shadow focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 text-lg">
-            <span className="block truncate">{currencies[selected]}</span>
+            <span className="block truncate text-ellipsis overflow-hidden">{`${selected} - ${currencies[selected]}`}</span>
             <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
               <SelectorIcon className="h-5 w-5 text-gray-400" aria-hidden="true" />
             </span>
           </Listbox.Button>
           <Transition as={Fragment} leave="transition ease-in duration-100" leaveFrom="opacity-100" leaveTo="opacity-0">
-            <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+            <Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none z-10 sm:text-sm">
               {Object.keys(currencies).map((currency, currencyIdx) => (
                 <Listbox.Option
                   key={currencyIdx}
-                  className={({ active }) => `relative cursor-default select-none py-2 pl-10 pr-4 ${active ? 'bg-amber-100 text-amber-900' : 'text-gray-900'}`}
+                  className={({ active }) => `relative cursor-default select-none py-2 pl-10 pr-4 ${active ? 'bg-emerald-100 text-emerald-900' : 'text-gray-900'}`}
                   value={currency}
                 >
                   {({ selected }) => (
                     <>
-                      <span className={`block truncate ${selected ? 'font-medium' : 'font-normal'}`}>{currencies[currency]}</span>
+                      <span className={`block truncate ${selected ? 'font-medium' : 'font-normal'}`}>{`${currency} - ${currencies[currency]}`}</span>
                       {selected ? (
-                        <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600">
+                        <span className="absolute inset-y-0 left-0 flex items-center pl-3 text-emerald-600">
                           <CheckIcon className="h-5 w-5" aria-hidden="true" />
                         </span>
                       ) : null}
